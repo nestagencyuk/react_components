11c4d8eb65828ef70554a0fa0c33544aae0e48dc
Milly <work@millybillins.com>
2020-02-25 11:51:06 +0000

    Update

--- dist/components/Icon/types.d.ts
+++ dist/components/Icon/types.d.ts
@@ -1,10 +1,9 @@
 declare namespace IIcon {
-  interface IProps {
-    className?: string
-    name: string
-    size?: 'XSmall' | 'Small' | 'Large'
-    colour?: 'Dark' | 'Light' | 'Success' | 'Warning' | 'Error'
-  }
+    interface IProps {
+        className?: string;
+        name: string;
+        size?: 'XSmall' | 'Small' | 'Large';
+        colour?: 'Dark' | 'Light' | 'Success' | 'Warning' | 'Error';
+    }
 }
-
-export default IIcon
+export default IIcon;
