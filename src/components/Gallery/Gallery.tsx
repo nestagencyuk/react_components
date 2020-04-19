0aa849023ff0d0876ce06f9e35257bad1994e7de
Milly Billins <work@millybillins.com>
2020-04-19 16:46:26 +0100

    Improve masonry grid

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,6 +1,6 @@
 import { IGallery } from './types'
 import * as React from 'react'
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef, createRef } from 'react'
 import cx from 'classnames'
 
 /**
@@ -29,71 +29,91 @@ const variants = {
 }
 
 /**
+ * Masonry magic
+ */
+const masonry = (wrapper: any, items: any) => {
+  if (!wrapper) return
+  const gap = window.getComputedStyle(wrapper).getPropertyValue('grid-column-gap')
+
+  items.forEach((x: any) => {
+    const item = x.current.firstChild as HTMLElement
+    console.log(parseInt(gap))
+    x.current.style.gridRowEnd = `span ${item.clientHeight + parseInt(gap)}`
+  })
+}
+
+/**
+ * Lightbox style
+ */
+const LightBoxGallery = ({ className, variant, items }: IGallery.IProps) => (
+  <Toggle>
+    {({ toggled, setToggled }: any) => {
+      const ref = useRef<HTMLElement>()
+      const refs = useRef<Array<React.RefObject<HTMLDivElement>>>(Array.from(Array(typeof items === 'function' && items({}).length).keys()).map(() => createRef()))
+      const [init, setInit] = useState(0)
+
+      /**
+       * Toggle the slider visibility
+       */
+      const handleClick = (i: number) => {
+        setToggled(true)
+        setInit(i)
+      }
+
+      const childItems = typeof items === 'function' && items({ handleClick })
+
+      /**
+       * Determine if masonry grid and resize
+       */
+      useEffect(() => {
+        if (variant !== 'Masonry') return
+        masonry(ref.current, refs.current)
+        window.addEventListener('resize', () => masonry(ref.current, refs.current))
+      }, [])
+
+      return (
+        <section ref={ref} className={cx(className, 'gallery', variants[variant])}>
+          {toggled && (
+            <Float className='gallery__float' portal align={{ x: 'Center', y: 'Center' }}>
+              <Overlay portal onClick={() => setToggled(false)} />
+              <Slider className='gallery__slider' variant='Fade' init={init} nav='Buttons' items={childItems} />
+            </Float>
+          )}
+
+          {childItems.map((x: any, i: any) => (
+            <div ref={refs.current[i]} key={`gallery-item-${i}`} className='gallery__item'>
+              {x}
+            </div>
+          ))}
+        </section>
+      )
+    }}
+  </Toggle>
+)
+
+/**
  * A gallery of items
  */
 const Gallery = ({ className, variant = 'Tiles', items, lightbox }: IGallery.IProps) => {
   const ref = useRef<HTMLElement>()
-
-  /**
-   * Masonry magic
-   */
-  const masonry = () => {
-    const items = ref.current.querySelectorAll('.gallery__item')
-
-    for (let x = 0; x < items.length; x++) {
-      const item = items[x] as HTMLElement
-      const rowHeight = parseInt(window.getComputedStyle(ref.current).getPropertyValue('grid-auto-rows'))
-      const rowGap = parseInt(window.getComputedStyle(ref.current).getPropertyValue('grid-row-gap'))
-      const innerItem = item.querySelector('.img')
-      const rowSpan = Math.ceil((innerItem.getBoundingClientRect().height + rowGap) / (rowHeight + rowGap))
-      item.style.gridRowEnd = `span ${rowSpan}`
-    }
-  }
+  const refs = useRef<Array<React.RefObject<HTMLDivElement>>>(Array.from(Array(items.length).keys()).map(() => createRef()))
 
   /**
    * Determine if masonry grid and resize
    */
   useEffect(() => {
     if (variant !== 'Masonry') return
-    masonry()
-    window.addEventListener('resize', masonry)
+    masonry(ref.current, refs.current)
+    window.addEventListener('resize', () => masonry(ref.current, refs.current))
   }, [])
 
   return lightbox ? (
-    <Toggle>
-      {({ toggled, setToggled }: any) => {
-        const [init, setInit] = useState(0)
-
-        const handleClick = (i: number) => {
-          setToggled(true)
-          setInit(i)
-        }
-
-        const childItems = typeof items === 'function' && items({ handleClick })
-
-        return (
-          <section ref={ref} className={cx(className, 'gallery', variants[variant])}>
-            {toggled && (
-              <Float className="gallery__float" portal align={{ x: 'Center', y: 'Center' }}>
-                <Overlay portal onClick={() => setToggled(false)} />
-                <Slider className="gallery__slider" variant="Fade" init={init} nav="Buttons" items={childItems} />
-              </Float>
-            )}
-
-            {childItems.map((x: any, i: any) => (
-              <div key={`gallery-item-${i}`} className="gallery__item">
-                {x}
-              </div>
-            ))}
-          </section>
-        )
-      }}
-    </Toggle>
+    <LightBoxGallery className={className} variant={variant} items={items} />
   ) : (
-    <section className={cx(className, 'gallery', variants[variant])}>
+    <section ref={ref} className={cx(className, 'gallery', variants[variant])}>
       {Array.isArray(items) &&
         items.map((x: any, i: any) => (
-          <div key={`gallery-item-${i}`} className="gallery__item">
+          <div ref={refs.current[i]} key={`gallery-item-${i}`} className='gallery__item'>
             {x}
           </div>
         ))}
