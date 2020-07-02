7a8988b258f74e47b3214226063416b38e6f7ff1
Milly Billins <work@millybillins.com>
2020-07-02 18:02:36 +0100

    Upgrade Storybook and use proper decorators

--- .storybook/decorators.js
+++ .storybook/decorators.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 /**
  * Demo Decorator
  */
-const demoDecorator = (story) => <div className="demo">{story()}</div>
+const demoDecorator = story => <div className="demo">{story()}</div>;
 
 /**
  * Demo and Name Decorator
  */
-const nameDecorator = (story) => <div className="demo">{story()}<span className="demo__caption">{story().props.name}</span></div>
+const nameDecorator = story => (
+  <div className="demo">
+    {story()}
+    <span className="demo__caption">{story().props.name}</span>
+  </div>
+);
 
-export { demoDecorator, nameDecorator }
+/**
+ * Force a large container for scrollable demos
+ */
+const scrollDecorator = story => (
+  <div className="demo" style={{ height: '1000px' }}>
+    {story()}
+  </div>
+);
+
+export { demoDecorator, nameDecorator, scrollDecorator };
