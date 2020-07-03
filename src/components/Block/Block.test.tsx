9448c5a013f36451ff8501993f814441feeabe5f
Jamie <jamiemmarsay@gmail.com>
2020-07-03 11:37:59 +0100

    update content for block tests

--- src/components/Block/Block.test.tsx
+++ src/components/Block/Block.test.tsx
@@ -8,7 +8,8 @@ describe('----- Block Component -----', () => {
   const baseProps: IBlock.IProps = {
     image: { align: 'Start', src: 'https://source.unsplash.com/random/1000x1000', alt: 'Block Image', aspect: '1x1' },
     header: { heading: 'Heading', subheading: 'Sub Heading' },
-    children: 'Testtesttest'
+    children:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
   };
 
   it('Renders without crashing', () => {
