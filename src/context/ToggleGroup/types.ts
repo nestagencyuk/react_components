43917157993fcadcee683e5b710bbcd7d4f5cee9
Milly Billins <work@millybillins.com>
2020-07-17 23:51:38 +0100

    Update docs

--- src/context/ToggleGroup/types.ts
+++ src/context/ToggleGroup/types.ts
@@ -2,8 +2,8 @@ declare namespace IToggleGroup {
   interface IProps {
     multi?: boolean
     children:
-    | React.ReactNode
-    | ((value: { toggles: { [key: string]: boolean }, setToggled: (id: string) => void }) => React.ReactNode)
+      | React.ReactNode
+      | ((value: { toggles: { [key: string]: boolean }; setToggled: (id: string) => void }) => React.ReactNode)
   }
 }
 
--- src/context/ToggleTree/types.ts
+++ src/context/ToggleTree/types.ts
@@ -1,20 +1,20 @@
-import { IUseToggleTree } from '../../hooks/useToggleTree/types';
+import { IUseToggleTree } from '../../hooks/useToggleTree/types'
 
 declare namespace IToggleTree {
   interface IProps {
-    multi?: boolean;
+    multi?: boolean
     children:
       | React.ReactNode
       | ((value: {
-          toggles: { [key: string]: { open: boolean; depth: number } };
-          setToggled: (id: string, depth?: number) => void;
-        }) => React.ReactNode);
+          toggles: { [key: string]: { open: boolean; depth: number } }
+          setToggled: (id: string, depth?: number) => void
+        }) => React.ReactNode)
   }
 
   interface IValue {
-    toggles: IUseToggleTree.IState;
-    setToggled: (id: string, depth?: number) => void;
+    toggles: IUseToggleTree.IState
+    setToggled: (id: string, depth?: number) => void
   }
 }
 
-export { IToggleTree };
+export { IToggleTree }
