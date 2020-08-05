de1b44d461c08c25f5dd721ba9011465f08246ae
Jamie <jamiemmarsay@gmail.com>
2020-08-05 18:37:47 +0100

    adds pagination updates as per milly's feedback

--- src/components/DataTable/DataTableFooter.tsx
+++ src/components/DataTable/DataTableFooter.tsx
@@ -12,6 +12,7 @@ import './DataTable.scss'
  */
 import { Grid, GridItem } from '../Grid'
 import { Button } from '../Button'
+import { Box } from '../Box'
 import { Text } from '../Text'
 import { Input } from '../Input'
 import { IDataTable } from './types'
@@ -30,52 +31,39 @@ const DataTableFooter: React.FC<IDataTable.IFooterControls> = ({ displayRowCount
     <footer className="datatable-footer" data-testid="datatable-footer">
       <Grid vCentred>
         {displayRowCount && (
-          <GridItem span={2}>
+          <GridItem span={4}>
             <Text className="text--bold">{`${rowCount} ${rowCount === 1 ? 'Line' : 'Lines'}`}</Text>
           </GridItem>
         )}
         {displayPagination && (
-          <GridItem span={10} className="m--l-auto">
-            <Grid>
-              <GridItem className="m--r-xs" span={4}>
-                <Button
-                  className="w--100"
-                  disabled={currentPage === 1 || currentPage === 0}
-                  onClick={() => prevPage()}
-                  variant="Tertiary"
-                >
-                  Prev
-                </Button>
-              </GridItem>
-              <GridItem span={4}>
-                <Grid vCentred>
-                  <GridItem className="m--r-xs" span={6}>
-                    <Input
-                      id="paginationJump"
-                      type="Number"
-                      className="text--bold w--100"
-                      value={currentPage || 1}
-                      minValue={1}
-                      maxValue={lastPage || 1}
-                      onChange={handlePaginationJump}
-                    />
-                  </GridItem>
-                  <GridItem span={6}>
-                    <Text className="text--bold">of {lastPage}</Text>
-                  </GridItem>
-                </Grid>
-              </GridItem>
-              <GridItem span={4}>
-                <Button
-                  disabled={rowCount === 0 ? true : currentPage === lastPage}
-                  className="w--100"
-                  onClick={() => nextPage()}
-                  variant="Tertiary"
-                >
-                  Next
-                </Button>
-              </GridItem>
-            </Grid>
+          <GridItem span={8} className="m--l-auto">
+            <Button
+              className="m--r-xs"
+              disabled={currentPage === 1 || currentPage === 0}
+              onClick={() => prevPage()}
+              variant="Tertiary"
+            >
+              Prev
+            </Button>
+            <Input
+              id="paginationJump"
+              type="Number"
+              className="m--r-xs"
+              value={currentPage || 1}
+              minValue={1}
+              maxValue={lastPage || 1}
+              onChange={handlePaginationJump}
+            />
+            <Text tag="span" className="text--bold m--r-xs">
+              of {lastPage}
+            </Text>
+            <Button
+              disabled={rowCount === 0 ? true : currentPage === lastPage}
+              onClick={() => nextPage()}
+              variant="Tertiary"
+            >
+              Next
+            </Button>
           </GridItem>
         )}
       </Grid>
