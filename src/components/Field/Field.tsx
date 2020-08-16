c17856d228417763fa8c0a5ed4c2b942970210df
Jamie <jamiemmarsay@gmail.com>
2020-08-16 20:49:20 +0100

    adds updates to field component, datepicker types and fixes minor bugs

--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -11,7 +11,6 @@ import './Field.scss'
 /**
  * Components
  */
-import { Label } from '../Label'
 import { Icon } from '../Icon'
 import { FieldPicker } from '.'
 
@@ -38,21 +37,27 @@ const icons: IIcon.IIconTypes = {
 /**
  * Field wrapper component
  */
-const Field: React.FC<IField.IProps> = ({ className, label, state, msg, ...props }) => (
-  <div className={cx(className, 'field', states[state])}>
-    {label && (
-      <Label
-        className={cx('field__label', { 'field__label--interactive': props.type === 'Checkbox' || props.type === 'Radio' })}
-        for={props.id}
-      >
-        {label}
-      </Label>
-    )}
-    <FieldPicker state={state} {...props} />
+const Field: React.FC<IField.IProps> = ({ className, label, state, msg, type, ...props }) => {
+  console.log(type)
 
-    {state && <Icon className={'field__icn'} name={icons[state]} colour={state} />}
-    {msg && <p className={'field__msg'}>{msg}</p>}
-  </div>
-)
+  return (
+    <div className={cx(className, 'field', states[state])}>
+      {label && (
+        <label
+          className={cx('field__label m--b-md text--bold', {
+            'field__label--interactive': type === 'Checkbox' || type === 'Radio'
+          })}
+          htmlFor={props.id}
+        >
+          {label}
+        </label>
+      )}
+      <FieldPicker state={state} type={type} {...props} />
+
+      {state && <Icon className={'field__icn'} name={icons[state]} colour={state} />}
+      {msg && <p className={'field__msg'}>{msg}</p>}
+    </div>
+  )
+}
 
 export default Field
